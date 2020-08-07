9f921df46ef3cab9d47c61268a1c7e4e17de7f8a
Jamille Veiga <jal.cveiga@gmail.com>
2020-08-06 22:16:01 -0300

    Initial commit

---
+++ Calc_Surface_Metrics.R
@@ -0,0 +1,61 @@
+## Calculate surface metrics
+
+## Clean Global Environment 
+rm(list=ls())
+
+# load libraries
+library(geodiv)
+library(raster)
+library(sp)
+library(tidyverse)
+
+## Load source
+source("source_gsm_geodiv.R")
+
+#### Set projection settings before running the script ####
+A=make_EPSG()
+LL_prj <- as.character(subset(A, A$code=="4326")[3]) 
+UTM_prj <- as.character(subset(A, A$code=="5383")[3]) 
+
+## Set names
+project_name = "charinus"
+raster_name = "elevation"
+
+#### Load data ####
+# # load the clippings
+clippings_elev <- readRDS("./ellipsoid_landscapes/clippings_flatten_elevation.rds")
+
+# load sampling points
+sampling_points <- raster::shapefile("coords/cha/charinus_shape_27samples.shp")
+crs(sampling_points)
+sampling_points <- spTransform(sampling_points, CRSobj = CRS(UTM_prj))
+crs(sampling_points)
+
+#### Specify metrics ####
+## Make list to choose which surface metrics to calculate
+## Not running yet
+
+#suface_metric_sub <- c("sa", 
+#                       "ssk", 
+#                       "std", 
+#                       "stdi")
+
+
+# Calculate metrics locally but overall printing progress
+#gsm <- calculate_gsm(clippings_elev)
+#save(gsm, file="./gsm_results/gsm_elevation.RData")
+load(file="./gsm_results/gsm_elevation.RData")
+
+# Bind to one dataframe
+gsm_data <- dplyr::bind_rows(gsm)
+head(gsm_data)
+nrow(gsm_data)
+
+gsm_data$sfd
+
+## Save data
+write.csv(gsm_data, paste0("./gsm_results/gsm_data_", raster_name, "_", project_name, ".csv"), row.names = F)
+
+## Obs: ainda preciso voltar com as colunas de IDs das cavernas, e depois fazer a correspondência para IDs dos indivíduos (copiar os mesmsos valores para os pares de cavernas repetidos).
+
+##The end
